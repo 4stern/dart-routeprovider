9f78ba48886faf26aa58d8ed2ba9c972ff9a3968
4stern <4sternrb@googlemail.com>
2015-05-12 21:22:04 +0200

    F1 - add error controller to test

--- test/route_provider_test.dart
+++ test/route_provider_test.dart
@@ -2,6 +2,16 @@ library route_provider;
 
 import 'dart:io';
 import 'package:route_provider/route_provider.dart';
+import 'dart:async';
+
+class RouteControllerError extends RouteController {
+    RouteControllerError();
+
+    Future<Map> execute(HttpRequest request, Map params) async  {
+        throw new RouteError(HttpStatus.NOT_FOUND,"ERROR");
+    }
+
+}
 
 void main() async {
 
@@ -17,6 +27,11 @@ void main() async {
     ..route({
         "url": "/",
         "controller": new RouteControllerEmpty(),
+        "response": new FileResponse("docroot/home.html")
+    })
+    ..route({
+        "url": "/error",
+        "controller": new RouteControllerError(),
         "response": new FileResponse("docroot/home.html")
     })
     ..start();
